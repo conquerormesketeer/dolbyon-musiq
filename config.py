16314750ba13d34c3a21f48fba8ff04445070d36
♗CσNϙUҽRσR_MҽSƙEƚҽҽR <79249487+conquerormesketeer@users.noreply.github.com>
2021-12-04 23:56:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
+admins = {}
+OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_UNAME = getenv("OWNER_NAME")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
